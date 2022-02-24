01fc6fcc3b6ffd0228b677936223086954c248cf
Rohit <rohitkamble10792@gmail.com>
2022-02-24 09:09:29 +0530

    First commit

---
+++ src/com/blz/GamblingGame/GamblingGame.java
@@ -0,0 +1,11 @@
+package com.blz.GamblingGame;
+
+public class GamblingGame {
+
+    public static final int STAKE_COST = 100;
+    public static final int BET_COST = 1;
+
+    public static void main(String[] args) {
+
+    }
+}
