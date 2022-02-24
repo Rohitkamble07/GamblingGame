576e00662e94af8151a366b4048ecc1f8e4d808f
Rohit <rohitkamble10792@gmail.com>
2022-02-24 09:28:39 +0530

    Added:Functionality of Win or Loose

--- src/com/blz/GamblingGame/GamblingGame.java
+++ src/com/blz/GamblingGame/GamblingGame.java
@@ -6,6 +6,11 @@ public class GamblingGame {
     public static final int BET_COST = 1;
 
     public static void main(String[] args) {
+        int bet = (int) Math.floor(Math.random() * 10) % 2;
+        if (bet == 1)
+            System.out.println("Win");
+        else
+            System.out.println("loose");
 
     }
 }
